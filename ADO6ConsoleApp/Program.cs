7f9bbe401a95266a0cda5bb28565a9979be95303
nonhlanhla-tshimologong <nonhlanhla@dsa-tshimologong.joburg>
2022-05-13 09:55:54 +0200

    method2

---
+++ ADO6ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AccessLayer;
+namespace AD06ConsoleApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is a connection string which is used to store the details.
+            string connect = "Server=LAPTOP-K8GSBMR5; Database=BethanysPieShop;Trusted_Connection=true;";
+
+            //access the function from the access layer
+            Access.Connect(connect);
+           
+        }
+    }
+}
+
---
+++ AccessLayer/Access.cs
@@ -0,0 +1,62 @@
+﻿using System.Data.SqlClient;
+namespace AccessLayer
+{
+    public class Access
+    {
+        // method grabs string input
+        //create a method that would connect to the database
+        public static void Connect(string connectionString)
+        {
+            //create connection
+            SqlConnection con = new SqlConnection(connectionString);
+          
+
+
+
+            ////create query that writes to database
+            //string sql = "Insert Into YourTabele(columnName, columnName) values ('value1', value2)";
+
+            ////open connection
+            //con.Open();
+
+            ////create a sql command referencing the connection
+            //SqlCommand cmd = new SqlCommand(sql, con);
+            //cmd.ExecuteNonQuery();
+
+            ////close connection
+            //con.Close();
+
+            //open connection
+            con.Open();
+            //create query that reads from database
+            string sql2 = "SELECT * FROM Category";
+            //create a sql command referencing the connection
+            SqlCommand cmd2 = new SqlCommand(sql2, con);
+
+            //create sql data reader
+            SqlDataReader dataReader = cmd2.ExecuteReader();
+
+            //while its reading
+            while (dataReader.Read())
+            {
+                //store the columns in variables
+              
+                var itemName = dataReader["CategoryName"].ToString();
+                var itemPrice = dataReader["Description"].ToString();
+
+                //write to console
+                Console.WriteLine(itemName + " " + itemPrice);
+
+
+            }
+            //close connection
+            con.Close();
+            //dispose of connection
+            con.Dispose();
+
+
+
+
+        }
+    }
+}
