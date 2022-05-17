d9dd0046f774fc7180b2fd123cbef9d0a811266a
nonhlanhla-tshimologong <nonhlanhla@dsa-tshimologong.joburg>
2022-05-17 11:19:07 +0200

    final connection

---
+++ selfADOconnection/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AccessLayer2;
+namespace selfADOconnection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connect = "Server=LAPTOP-K8GSBMR5; Database=BethanysPieShop; Trusted_Connection=true";
+
+            Access2.Connect(connect);
+        }
+    }
+
+}
