6ec061f6b1514e691cfcdd4f2d331198387f1a10
nonhlanhla-tshimologong <nonhlanhla@dsa-tshimologong.joburg>
2022-05-17 14:48:34 +0200

    using statement and declaration

---
+++ Accessing/AccessClass.cs
@@ -0,0 +1,57 @@
+﻿using System.Data.SqlClient;
+namespace Accessing
+{
+    public class AccessClass
+    {
+        public static void Connect(string conn)
+        {
+            string manyLines = @"This is line one
+                                 This is line two
+                                 Here is line three
+                                 The penultimate line is line four
+                                 This is the final, fifth line";
+
+            //using (var reader = new StringReader(manyLines))
+            //{
+            //    string item;
+            //    do
+            //    {
+            //        item = reader.ReadLine();
+            //        Console.WriteLine(item);
+            //    }
+            //    while (item != null);
+            //}
+
+            ////method 2
+            //using var reader = new StringReader(manyLines);
+            //string? item;
+            //do
+            //{
+            //    item = reader.ReadLine();
+            //    Console.WriteLine(item);
+            //}
+            //while(item != null);
+
+            //method 3
+
+            {
+                var reader = new StringReader(manyLines);
+                try
+                {
+                    string? item;
+                    do
+                    {
+                        item = reader.ReadLine();
+                        Console.WriteLine(item);
+                    }
+                    while (item != null);
+                }
+                finally
+                {
+                    reader.Close();
+                }
+            }
+
+        }
+    }
+}
---
+++ usingStatementDeclaration/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Accessing;
+namespace usingStatementDeclaration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connect = "Server = ; Database = ; Trusted_Connection=true;";
+
+            AccessClass.Connect(connect);
+        }
+    }
+}
